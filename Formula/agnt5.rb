0dcd57eeb1c29c25d1994414bf22b4779b00d746
Arun Reddy <arunreddy.nelakurthi@gmail.com>
2025-09-25 08:11:13 -0700

    Initial commit

---
+++ Formula/agnt5.rb
@@ -0,0 +1,23 @@
+class Agnt5 < Formula
+  desc "TODO: Add description for agnt5"
+  homepage "https://agnt5.dev"
+  url "TODO: Add download URL"
+  sha256 "TODO: Add SHA256 checksum"
+  license "TODO: Add license"
+
+  # TODO: Add any dependencies here
+  # depends_on "python@3.11"
+
+  def install
+    # TODO: Add installation instructions
+    # Example for a Python package:
+    # bin.install "agnt5"
+    # Or for source builds:
+    # system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    # TODO: Add test to verify installation
+    # system "#{bin}/agnt5", "--version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,21 @@
+# homebrew-agnt5
+
+Custom Homebrew tap for agnt5.
+
+## Installation
+
+```bash
+# Add the tap
+brew tap arun/agnt5
+
+# Install agnt5
+brew install agnt5
+```
+
+## Usage
+
+After installation, you can use agnt5 by running:
+
+```bash
+agnt5 --help
+```
